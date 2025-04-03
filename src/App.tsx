933490f261908106e98f8b2076b8921cda0c4d97
aldoalbuquerquejr <aldocandeia000@gmail.com>
2025-04-03 20:37:37 -0300

    setting up frontend environment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='p-4 text-center text-xl font-bold text-amber-500'>Este é o início de um projeto de uma aplicação web para gerenciamento de uma barvearia!
+      </h1>
+    </>
+  )
+}
+
+export default App
